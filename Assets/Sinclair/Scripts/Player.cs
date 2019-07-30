a80232a3f183fc5f3d5a21c8524653baf322e206
Sinclair <sinfram248@gmail.com>
2019-07-30 12:10:02 +1000

    Implemented second joystick and auto-shooting

--- Assets/Sinclair/Scripts/Player.cs
+++ Assets/Sinclair/Scripts/Player.cs
@@ -8,9 +8,11 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	// External references
-	public Joystick joystick;
+	public Joystick movementStick;
+    public Joystick aimingStick;
 	public GameObject bulletPrefab;
 	public Transform shotOrigin;
+    public Transform turret;
 
 	// Internal references
 	Rigidbody rb;
@@ -19,7 +21,9 @@ public class Player : MonoBehaviour {
 	float shootTimer = 0f;
 	float shootRate = .1f;
 	float speed = 2f;
-	float movementThreshold = 0.25f;
+	float movementThreshold = 0.2f;
+    float aimingThreshold = 0.2f;
+    Vector3 turretDirection;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
@@ -27,7 +31,7 @@ public class Player : MonoBehaviour {
     
 	void Move() {
 		// Gets input from joystick
-		Vector2 input = new Vector2(joystick.Horizontal, joystick.Vertical);
+		Vector2 input = new Vector2(movementStick.Horizontal, movementStick.Vertical);
 		// If input is less than movement threshold, set input to zero
 		if (input.x * input.x + input.y * input.y < movementThreshold * movementThreshold) {
 			input = input.normalized*0.001f;
@@ -40,13 +44,22 @@ public class Player : MonoBehaviour {
 		if (input != Vector2.zero) {
 			transform.forward = rb.velocity;
 		}
+        Vector2 aim = new Vector2(aimingStick.Horizontal, aimingStick.Vertical);
+        if (aim != Vector2.zero) {
+            turretDirection = new Vector3(aim.x, 0f, aim.y);
+        }
+        turret.forward = turretDirection;
+        if (aim.x * aim.x + aim.y * aim.y > aimingThreshold * aimingThreshold)
+        {
+            Shoot();
+        }
 	}
 
-	public void Shoot() {
+	void Shoot() {
 		if (shootTimer <= 0f) {
 			shootTimer += shootRate;
 
-			Instantiate(bulletPrefab, shotOrigin.position, Quaternion.identity).transform.forward = transform.forward;
+			Instantiate(bulletPrefab, shotOrigin.position, Quaternion.identity).transform.forward = turret.forward;
 		}
 	}
 
