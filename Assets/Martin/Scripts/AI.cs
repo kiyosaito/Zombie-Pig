055a8d1f6493d2cdbf99a03d2b0e7dd877afd3ac
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-08-07 11:57:12 +1000

    AI
    Changing directio AI will be done to  NAVMESH not astar

--- Assets/Martin/Scripts/AI.cs
+++ Assets/Martin/Scripts/AI.cs
@@ -1,24 +1,47 @@
-﻿// Title: 
+﻿// Title: AI
 // Author(s): Martin Nguyen
-// Date: 
+// Date: 7/08/19
 // Details: 
 // Reference: 
 // =========================================================
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private NavMeshAgent _navMeshAgent;
+    public GameObject player;
+    public float attackDistance = 10f;
+    public float followDistance = 20f;
 
-    // Update is called once per frame
+    public Transform[] patrolPoints;
+    private int currentPointIndex = 0;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+       
+    }
+    
+
     void Update()
     {
-        
+        if (_navMeshAgent.enabled)
+        {
+            float dist = Vector3.Distance(player.transform.position, this.transform.position);
+            bool patrol false;
+        }
+    }
+    void NextPatrolPoint()
+    {
+        if (patrolPoints.Length>0)
+        {
+            _navMeshAgent.destination = patrolPoints[currentPointIndex].position;
+            currentPointIndex++;
+            currentPointIndex %= patrolPoints.Length;
+
+        }
     }
 }
